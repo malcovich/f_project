97bed8d84ed0254c6cc9ccbed7e49bf6ba7302ef
Vertii, Dmytro (US - Hermitage) <dvertii@deloitte.com>
2018-06-21 17:34:23 +0300

    first commit

---
+++ server/app.ts
@@ -0,0 +1,41 @@
+import * as express from 'express';
+import * as morgan from 'morgan';
+import * as mongoose from 'mongoose';
+import * as path from 'path';
+
+import setRoutes from './routes';
+
+const app = express();
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let mongodbURI;
+if (process.env.NODE_ENV === 'test') {
+  mongodbURI = process.env.MONGODB_TEST_URI;
+} else {
+  mongodbURI = process.env.MONGODB_URI;
+  app.use(morgan('dev'));
+}
+
+mongoose.Promise = global.Promise;
+// mongoose.connect(mongodbURI)
+mongoose.connect("mongodb://admin:Malcovich1927@ds163730.mlab.com:63730/f_project")
+  .then(db => {
+    console.log('Connected to MongoDB');
+
+    setRoutes(app);
+
+    app.get('/*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+    if (!module.parent) {
+      app.listen(app.get('port'), () => console.log(`Angular Full Stack listening on port ${app.get('port')}`));
+    }
+  })
+  .catch(err => console.error(err));
+
+export { app };
